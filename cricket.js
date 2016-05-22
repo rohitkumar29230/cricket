50c32e2727afae3cdcfde379ad514792f54d4164
Dheeraj Kumar <rohitkumar2930@gmail.com>
2016-05-22 12:43:38 +0530

    change in js

--- cricket.js
+++ cricket.js
@@ -1,11 +1,5 @@
 $(document).ready(function(){
-    $("#test").hover(function(){
-        $.ajax({
-			url: 'http://localhost/database.json',
-            success: function(res){
-                console.log(res)
-            }
-		
-		})
+    $("#intro").mouseover(function(){
+       alert(true)
     })
 })
