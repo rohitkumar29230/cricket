8f9776dce96e8ce72d4709841a91d9180a5201e5
rohitk <rohitk@clavax.us>
2016-05-26 15:38:29 +0530

    udpated angular js

--- site/js/main2.js
+++ site/js/main2.js
@@ -2,43 +2,73 @@
  * Main AngularJS Web Application
  */
 var app = angular.module('cricketApp', [
-  'ngRoute'
+    'ngRoute'
 ]);
 
 /**
  * Configure the Routes
  */
 app.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider
-    // Home
-    .when("/", {templateUrl: "templates/landing.html", controller: "PageCtrl"})
-    // Teams Page
-    .when("/select-team", {templateUrl: "templates/teamlist.html", controller: "teamList"})
-    // else 404
-    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
-}]);
+        $routeProvider
+                // Home
+                .when("/", {templateUrl: "templates/landing.html", controller: "PageCtrl"})
+                // Teams Page
+                .when("/select-team", {templateUrl: "templates/teamlist.html", controller: "teamList"})
+                // else 404
+                .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+    }]);
 
 
-app.controller('teamList', function($scope, $http){
-    
-        $http.get('database.json')
-            .then(function($response){
-               var _teamArry = [];
-               $scope.teamsLst =  $response.data;
-               $scope.countr= 0;
-               angular.forEach($scope.teamsLst.Teams, function(_value, _index){
-                   _teamArry.push(_value.Name);
-               });
-               $scope.names = _teamArry;
-               $scope.my = { favorite: '----------' };
+app.controller('teamList', function ($scope, $http) {
+    var _teamArry = []; // Teams Array
+    var _counter = 0;
+    this.teamA = ''
+    this.teamb = ''
+    var teamPlayers = ''
 
+
+    /**
+     * function to fetch the records from the database
+     */
+    $http.get('database.json')
+            .then(function ($response) {
+                $scope.teamsLst = $response.data;
+                $scope.countr = 0;
+                $scope.allTeams = $scope.teamsLst.Teams; // creating scope for teams
             });
+
+    $scope.setTeam = function (_this) {
+        $scope.teamPlayers = $scope.allTeams;
+        angular.forEach($scope.allTeams, function (_value, _index) {
             
-            $scope.PlayersList =  function(){
-                $scope.msg = 'gone'
+            if (angular.equals(_value.ID, _this)) {
+                $scope.teamName =  _value.Name
+                $scope.teamPlayers = _value.Players;
             }
-            
+        });
+
+    }
+
+    $scope.showTeams = function () {
+        var _minTeamsPlayers = 22;
+        var _teamLenth = $('.players ul li').length;
+        
+        if (_teamLenth > 21) {
+            $('.teamList').addClass('ng-hide');
+            $('.players').removeClass('ng-hide');
+            $('.players').addClass('ng-show');
+             
+            $scope.msg = '';
+        } else {
+            console.log(21)
+            $scope.msg = 'Please select both the teams first';
+        }
+
+    }
+
 });
+
+
 
 
 
@@ -47,7 +77,7 @@ app.controller('teamList', function($scope, $http){
  * Controls the Blog
  */
 app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
-  console.log("Blog Controller reporting for duty.");
+    console.log("Blog Controller reporting for duty.");
 });
 
 /**
@@ -55,6 +85,8 @@ app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
  */
 app.controller('PageCtrl', function (/* $scope, $location, $http */) {
 
- $scope.my = 1;
+    $scope.my = 1;
 
 });
+
+
